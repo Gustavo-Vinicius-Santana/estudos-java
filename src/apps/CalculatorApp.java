f4341b2cd881b51d2ce2ff77b2fc539443ad6e9f
Gustavo_Vinicius_SX <gus.tec.dev@gmail.com>
2024-05-20 11:13:34 -0300

    Initial commit

---
+++ src/apps/CalculatorApp.java
@@ -0,0 +1,8 @@
+package apps;
+
+public class CalculatorApp {
+    public static void Start(String[] args){
+        System.out.print("calculadora");
+    }
+
+}
---
+++ src/models/Calculator.java
@@ -0,0 +1,7 @@
+package models;
+
+public class Calculator {
+    public static void Calc(String[] args){
+        System.out.print("metodos de calculos");
+    }
+}
